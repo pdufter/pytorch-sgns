92b79b8e121c6aca9c58deaea34386448b4062fd
Philipp Dufter <philipp.dufter@gmail.com>
2021-01-14 10:50:16 +0100

    Default message

---
+++ create_fake.py
@@ -0,0 +1,19 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", default=None, type=str, required=True, help="")
+    parser.add_argument("--output", default=None, type=str, required=True, help="")
+    args = parser.parse_args()
+
+    with open(args.input) as fin:
+        with open(args.output) as fout:
+            for line in fin:
+                to_write = " ".join(["::{}".format(word) for word in line.strip().split()]) + "\n"
+                fout.write(line)
+                fout.write(to_write)
+
+
+if __name__ == '__main__':
+    main()
